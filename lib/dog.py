be56602179a962bdbdb6e94a341e194452bb7284
Ruth <ruro122020@gmail.com>
2023-11-28 20:39:39 -0500

    create Dog class name and breed in progress

--- lib/dog.py
+++ lib/dog.py
@@ -13,26 +13,23 @@ APPROVED_BREEDS = [
 
 class Dog:
     def __init__(self, name="", breed=""):
-        self.name = name
-        self.breed = breed
+        self.set_name(name)
+        self.set_breed(breed)
 
     def get_name(self):
-        print('Get Name')
         return self._name
     
     def set_name(self, name):
         if (type(name) == str) and (1 <= len(name) <= 25):
-            print('Set Name')
             self._name = name
         else:
              print("Name must be string between 1 and 25 characters.")
 
     def get_breed(self):
-        print('Get Breed')
         return self._breed
     
     def set_breed(self, breed):
-        if APPROVED_BREEDS.count(breed) == 0:
+        if breed not in APPROVED_BREEDS:
             print("Breed must be in list of approved breeds.")
         else:
             self._breed = breed
@@ -42,5 +39,5 @@ class Dog:
    
 
 
-
+print(Dog())
 
