0392c0c922464b79eacf5f066ca0c5aa8ecd4437
Ruth <ruro122020@gmail.com>
2023-11-28 19:43:15 -0500

    add dog name and breed to Dog class in progress

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,35 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed=""):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        print('Get Name')
+        return self._name
+    
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            print('Set Name')
+            self._name = name
+        else:
+             print("Name must be string between 1 and 25 characters.")
+
+    def get_breed(self):
+        print('Get Breed')
+        return self._breed
+    
+    def set_breed(self, breed):
+        if APPROVED_BREEDS.count(breed) == 0:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = breed
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+   
+
+
+
+
